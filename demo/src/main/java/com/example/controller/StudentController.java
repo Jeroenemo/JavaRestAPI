de95ef1bada50f93fff7671a0af54514df459688
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-06-09 17:19:30 -0700

    full crud

--- demo/src/main/java/com/example/controller/StudentController.java
+++ demo/src/main/java/com/example/controller/StudentController.java
@@ -1,16 +1,79 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Student;
+import com.example.request.CreateStudentRequest;
+import com.example.request.UpdateStudentRequest;
+import com.example.response.StudentResponse;
+//import com.example.response.StudentResponse;
+import com.example.service.StudentService;
 
 @RestController
 @RequestMapping("/api/student/")
 public class StudentController {
 	
-	@GetMapping("/get")
-//	@RequestMapping(value = "/get", method = RequestMethod.GET)
-	public String getStudent () {
-		return "Hello Student";
+	@Value("${app.name:Default Demo App}")
+	private String appName;
+	
+	@Autowired
+	StudentService studentService;
+	
+	@GetMapping("getAll")
+	public List<StudentResponse> getAllStudents () {
+		List<Student> studentList = studentService.getAllStudents();
+		List<StudentResponse> studentResponseList = new ArrayList<StudentResponse>();
+		
+		studentList.stream().forEach(student -> {
+			studentResponseList.add(new StudentResponse(student));
+		});
+		
+		return studentResponseList;
 	}
+	
+	@PostMapping("create")
+	public Student createStudent (@Valid @RequestBody CreateStudentRequest createStudentRequest) {
+		Student student = studentService.createStudent(createStudentRequest);
+		
+		return student;
+	}
+	
+	@PutMapping("update")
+	public StudentResponse updateStudent (@Valid @RequestBody UpdateStudentRequest updateStudentRequest) {
+		Student student = studentService.updateStudent(updateStudentRequest);
+		
+		return new StudentResponse(student);
+	}
+	
+	@DeleteMapping("delete/{id}")
+	public String deleteStudent (@PathVariable long id) {
+		return studentService.deleteStudent(id);
+	}
+	
+//	@DeleteMapping("delete")
+//	public String deleteStudent (@RequestParam long id) {
+//		return studentService.deleteStudent(id);
+//	}
+	
+//	@GetMapping("/get")
+////	@RequestMapping(value = "/get", method = RequestMethod.GET)
+//	public StudentResponse getStudent () {
+//		StudentResponse studentResponse = new StudentResponse(1, "Jeroen", "van Seeters");
+//		return studentResponse;
+//	}
 }
---
+++ demo/src/main/java/com/example/request/UpdateStudentRequest.java
@@ -0,0 +1,21 @@
+package com.example.request;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateStudentRequest {
+
+	@NotNull(message = "ID is required")
+	private Long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+}
